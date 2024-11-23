f4a501f4f8ef8ec7d74615165d628a19c9001fb5
albahaveriku <albahaveriku@gmail.com>
2024-11-23 16:40:54 +0100

    Update magpie.config.js

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,13 +1,8 @@
 export default {
   experimentId: '1',
    serverUrl: 'https://magpie-demo.herokuapp.com/',
-  //serverUrl: 'https://cui-motr-new.herokuapp.com/',
-  // serverUrl: 'https://gentle-everglades-00112.herokuapp.com/',
   socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
-  // socketUrl: 'wss://gentle-everglades-00112.herokuapp.com/socket',
-  // this will be used in prolific mode
-  //completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1I4D2OB',
-  //contactEmail: 'cui.ding@uzh.ch',
+  
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'debug',
   language: 'en'
