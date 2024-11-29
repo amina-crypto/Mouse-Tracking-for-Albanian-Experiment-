814459bacf22ecbda8c54f9a9427b89a3acfa316
albahaveriku <albahaveriku@gmail.com>
2024-11-30 00:41:58 +0100

    components

---
+++ src/components/screens/InstructionScreen.vue
@@ -0,0 +1,60 @@
+<docs>
+This is a pre-built instruction screen, with limited functionality, but simpler to use.
+
+```vue
+<Experiment>
+    <InstructionScreen>
+      These are instructions for the participant.<br />
+      Below the instructions is a button to continue.
+    </InstructionScreen>
+
+    <InstructionScreen>
+      This is another instruction screen.<br />
+      Below the instructions is a button to continue.
+    </InstructionScreen>
+
+</Experiment>
+```
+</docs>
+
+<template>
+  <Screen v-bind="{ ...$attrs, ...$props }">
+    <Slide>
+      <div class="instructions">
+        <slot name="default" />
+      </div>
+      <button @click="$magpie.nextScreen()">Fillo
+      </button>
+    </Slide>
+  </Screen>
+</template>
+
+<script>
+import Screen from '../Screen';
+import Slide from '../Slide';
+
+export default {
+  name: 'InstructionScreen',
+  components: {
+    Slide,
+    Screen
+  },
+  props: {
+   title: {
+      type: String,
+      required: true,
+    },
+    buttonName: {
+      type: String,
+      default: "Start", // Fallback button text
+    }
+	}
+};
+</script>
+<style scoped>
+.instructions {
+  width: 600px;
+  margin: 0 auto;
+  text-align: justify;
+}
+</style>
